192a22068d2f3035d7a3640fbd738126ce15a36c
Cesar Noriega <c79.noriega@gmail.com>
2023-03-18 21:45:54 -0500

    Added CommandLiner in Spring boot application.

--- src/main/java/com/cn/springUploadingfiiles/SpringUploadingFiilesApplication.java
+++ src/main/java/com/cn/springUploadingfiiles/SpringUploadingFiilesApplication.java
@@ -1,10 +1,13 @@
 package com.cn.springUploadingfiiles;
 
 import com.cn.springUploadingfiiles.config.StorageProperties;
+import com.cn.springUploadingfiiles.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -12,6 +15,13 @@ public class SpringUploadingFiilesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringUploadingFiilesApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }
